1701da44f157e21986a6fb28ff66f710d1670339
Marco Cours <marcocours@macbook-pro-de-marco.home>
2019-11-28 21:38:35 +0100

    Final Project

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var menuMobile = document.querySelector('.header__navMobile');
-var iconMenu = document.querySelector('.header__iconMenu');
-var iconClose = document.querySelector('.header__closeMenu');
+var menuMobile = document.querySelector(".header__navMobile");
+var iconMenu = document.querySelector(".header__iconMenu");
+var iconClose = document.querySelector(".header__closeMenu");
 
-iconMenu.addEventListener('click', function(){
-    menuMobile.classList.add('visible');
+iconMenu.addEventListener("click", function() {
+  menuMobile.classList.add("is-visible");
 });
 
-iconClose.addEventListener('click', function(){
-    menuMobile.classList.remove('visible');
+iconClose.addEventListener("click", function() {
+  menuMobile.classList.remove("is-visible");
 });
